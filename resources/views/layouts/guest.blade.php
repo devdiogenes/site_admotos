7caca0b6e57b3f5ad4bfc347c34aa30e6e62548e
diogenes-souza-dev <diogenes.cruz2001@gmail.com>
2022-12-06 17:32:56 -0300

    Revert "Realizado downgrade na versão php"
    This reverts commit 1b763edbf63b4e40e723bc28a71cb74a66ba8fe3.
    
    Reverter 1b763ed

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,20 +1,39 @@
+@props(['title' => 'AD Motos'])
+
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="pt-br">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+        <meta name="keywords" content="moto, motos, atacado, peças, ad, admotos, hesla, alseh, bahia, motopeças, motoparts"/>
+        <meta name="description" content="Somos uma importadora de peças para motocicletas referência no mercado. Através da nossa empresa, você pode adquirir produtos de altíssima qualidade por um preço acessível."/>
+        <link rel="canonical" href="http://admotos.com.br"/>
+        <title>{{$title}}</title>
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-170739082-2"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-170739082-2');
+            gtag('config', 'UA-170739082-3'); 
+        </script>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
-        <div class="font-sans text-gray-900 antialiased">
+        <div class="text-black font-ubuntu font-light w-full flex flex-col items-center antialiased">
             {{ $slot }}
         </div>
     </body>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+use App\Http\Controllers\ContactController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,7 +18,10 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
+Route::view('/', 'inicio');
+Route::post('/enviar', [ContactController::class, 'enviar']);
+
+/*Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
@@ -36,3 +41,4 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+*/
